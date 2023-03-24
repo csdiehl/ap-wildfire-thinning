bf825168292453d77d0ac2bdb83a3bce9b278439
Caleb Diehl <cdiehl@ap.org>
2023-03-23 20:38:27 -0700

    really close - but off by some constant scale

--- src/js/visuals/wildfire_thinning/Map.js
+++ src/js/visuals/wildfire_thinning/Map.js
@@ -1,11 +1,18 @@
-import { geoMercator, geoPath, select, zoom, zoomIdentity } from 'd3'
+import { geoMercator, geoPath, path, select, zoom, zoomIdentity } from 'd3'
 import { tile } from 'd3-tile'
 import React, { useEffect, useRef, useState } from 'react'
 import useGeoData from '../../components/useGeoData'
+import firesheds_thinning from './firesheds_thinning.json'
+import { feature } from 'topojson-client'
 
 function getHillShade(x, y, z) {
   return `https://server.arcgisonline.com/ArcGIS/rest/services/Elevation/World_Hillshade/MapServer/tile/${z}/${y}/${x}.png`
 }
+
+const firesheds = feature(
+  firesheds_thinning,
+  firesheds_thinning.objects.firesheds_thinning
+).features
 
 const Map = ({
   width,
@@ -24,7 +31,6 @@ const Map = ({
     zoomLevel: 4096,
   })
   // const firesheds = useGeoData('exp_firesheds.json')
-
   const svgRef = useRef()
 
   useEffect(() => {
@@ -53,14 +59,11 @@ const Map = ({
         .tileSize(512)
 
     // generators
-    // const path = geoPath(projection)
+    const path = geoPath(projection)
 
     const zoomed = (transform) => {
-      console.log(transform)
       // make tiles
       const tiles = tiler(transform)
-
-      console.log(tiles)
 
       const image = select(document.getElementById('hillshade-tiles'))
       // select the empty image by id
@@ -79,24 +82,28 @@ const Map = ({
       projection
         .scale(transform.k / (2 * Math.PI))
         .translate([transform.x, transform.y])
+
+      const paths = select(svgRef.current).selectAll('.fireshed')
+      paths.data(firesheds).attr('d', (d) => path(d))
     }
 
     const svg = select(svgRef.current)
 
-    console.log(mapCenter)
-
-    svg.call(
-      zoomer.transform,
-      zoomIdentity
-        .translate(width / 2, height / 2)
-        .scale(-mapCenter.zoomLevel)
-        .translate(...projection(mapCenter.center))
-        .scale(-1)
-    )
+    svg
+      .transition()
+      .duration(750)
+      .call(
+        zoomer.transform,
+        zoomIdentity
+          .translate(width / 2, height / 2)
+          .scale(-mapCenter.zoomLevel)
+          .translate(...projection(mapCenter.center))
+          .scale(-1)
+      )
   }, [mapCenter, height, width])
 
   function zoomIn() {
-    centerMap({ center: [-118.04, 45.71], zoomLevel: 7000 })
+    centerMap({ center: [-115.04, 39.71], zoomLevel: 2 * 4096 })
   }
 
   return (
@@ -109,6 +116,16 @@ const Map = ({
     >
       <g id='map-content' cursor='pointer'>
         <g id='hillshade-tiles'></g>
+      </g>
+      <g id='fireshed-paths'>
+        {firesheds.map((d) => (
+          <path
+            className='fireshed'
+            stroke='red'
+            fill='blue'
+            key={d.properties.fireshed_code}
+          ></path>
+        ))}
       </g>
     </svg>
   )
